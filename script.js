e11c4947ddf4c62f70f78f5f74c157787cb4864c
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-11-05 00:26:06 +0000

    first push

---
+++ script.js
@@ -0,0 +1,12 @@
+ $(document).ready(function() {
+       /* $(".info").mouseenter(function(){
+            $(this).css("background-color", "orange");
+            $(this).css("border", "solid black 2px");
+            
+        });
+        
+        $(".info").mouseleave(function(){
+            $(this).css("background-color", "white");
+             $(this).css("border", "0px");
+        });*/
+  });
