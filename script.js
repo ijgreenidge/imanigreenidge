725d2c99a9d2dac294310f02ec8a00da6f5f354c
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-11-05 01:47:51 +0000

    made info visibility toggled

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
  $(document).ready(function() {
-       /* $(".info").mouseenter(function(){
-            $(this).css("background-color", "orange");
-            $(this).css("border", "solid black 2px");
-            
-        });
-        
-        $(".info").mouseleave(function(){
-            $(this).css("background-color", "white");
-             $(this).css("border", "0px");
-        });*/
-  });
+    $("#designHeading").click(function() {
+         $(".designHidden").toggle();
+     });
+
+      $("#websiteHeading").click(function() {
+         $(".websiteHidden").toggle();
+     });
+     
+      $("#clothingHeading").click(function() {
+         $(".clothingHidden").toggle();
+     });
+ });
